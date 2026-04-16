eb7057c8f4dc27d9da41b45ed48594b86c58e06d
nivedh-cloud <nivedh@github.com>
2026-04-16 16:25:25 +0530

    changed in vite

--- src/data/resumeData.ts
+++ src/data/resumeData.ts
@@ -18,11 +18,8 @@ export const resumeData = {
 
   experience: [
     {
-
-      //changes done 
       id: 1,
       company: "Orbcomm India Pvt Ltd",
-
       position: "Technical Lead – UI",
       duration: "March 2015 – Present",
       location: "India",
